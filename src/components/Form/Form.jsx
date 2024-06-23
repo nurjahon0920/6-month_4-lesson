29cc075d35d7ce8bc7a2b88f415bd17850b343cf
Nurjahon <166979887+n20090920@users.noreply.github.com>
2024-06-23 22:45:16 +0500

    stillarni ozroq to'g'irladim

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import "./Form.scss";
 const Form = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [Group, setGroup] = useState("Rus");
-  const [Check, setCheck] = useState("on");
+  const [Firstname, setFirstname] = useState("");
+  const [Lastname, setLastname] = useState("");
+  const [Group, setGroup] = useState("Group");
+  const [Check, setCheck] = useState("yes");
   const [data, setData] = useState([]);
   const handelSubmit = (e) => {
     e.preventDefault();
     let user = {
-      name: name,
-      email: email,
+      Firstname: Firstname,
+      Lastname: Lastname,
       Group: Group,
       Check: Check,
     };
     setData([...data, user]);
-    setName("");
-    setEmail("");
+    setFirstname("");
+    setLastname("");
     setGroup("");
     setCheck("");
   };
@@ -25,58 +25,112 @@ const Form = () => {
     setData(dataCard);
   };
   let dataItem = data?.map((el, id) => (
-    <div key={id}>
-      <p>{el.name}</p>
-      <p>{el.email}</p>
-      <p>{el.Group}</p>
-      <p>{el.Check}</p>
-      <button onClick={() => DeletData(id)}>Delete</button>
+    <div
+      key={id}
+      style={{
+        display: "flex",
+        alignItems: "start",
+        gap: "20px",
+      }}>
+      <p style={{ width: "200px" }}>{el.Firstname}</p>
+      <p style={{ width: "200px" }}>{el.Lastname}</p>
+      <p style={{ width: "200px" }}>{el.Group}</p>
+      <p style={{ width: "200px" }}>{el.Check}</p>
+      <button
+        onClick={() => DeletData(id)}
+        style={{
+          background: "#ff0000",
+          width: "100px",
+          height: "40px",
+          color: "#fff",
+          cursor: "pointer",
+          borderRadius: "5px",
+          marginLeft: "-50px",
+        }}>
+        Delete
+      </button>
     </div>
   ));
   return (
     <>
       <div className="form">
-        <form onSubmit={handelSubmit} className="d-flex g-20">
+        <form
+          onSubmit={handelSubmit}
+          className="d-flex"
+          style={{ marginTop: "10px" }}>
           <input
             type="text"
-            placeholder="Name"
+            placeholder="Firstname"
             className={"p-1"}
-            style={{ width: "200px" }}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            style={{
+              width: "200px",
+              marginRight: "20px",
+              padding: "5px",
+              borderRadius: "4px",
+              border: "1px solid #333",
+            }}
+            value={Firstname}
+            onChange={(e) => setFirstname(e.target.value)}
             required
           />
           <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className={"bg-info"}
-            style={{ width: "200px" }}
+            type="text"
+            placeholder="Lastname"
+            value={Lastname}
+            onChange={(e) => setLastname(e.target.value)}
+            style={{
+              width: "200px",
+              marginRight: "20px",
+              padding: "5px",
+              borderRadius: "4px",
+              border: "1px solid #333",
+            }}
             required
           />
           <select
-            name="Rus"
+            name="group"
             className=""
-            style={{ width: "200px" }}
-            id=""
+            style={{
+              width: "200px",
+              padding: "5px",
+              borderRadius: "4px",
+              border: "1px solid #333",
+            }}
             value={Group}
             onChange={(e) => setGroup(e.target.value)}>
-            <option value="React N58">React N58</option>
-            <option value="React N53">React N53</option>
-            <option value="React N50">React N50</option>
-            <option value="React N45">React N45</option>
+            <option value="React N58" id="1">
+              React N58
+            </option>
+            <option value="React N53" id="2">
+              React N53
+            </option>
+            <option value="React N50" id="3">
+              React N50
+            </option>
+            <option value="React N45" id="4">
+              React N45
+            </option>
           </select>
           <input
             type="checkbox"
-            value={Check ? "on" : "ok"}
+            value={Check ? "yes" : "no"}
             onChange={(e) => setCheck(e.target.value)}
             className=""
-            style={{ width: "20px" }}
+            style={{ width: "20px", marginLeft: "20px", cursor: "pointer" }}
           />
-          <button style={{ width: "200px" }}>Add</button>
+          <button
+            style={{
+              width: "100px",
+              background: "aqua",
+              borderRadius: "5px",
+              marginLeft: "150px",
+            }}>
+            Add
+          </button>
         </form>
-        <div className="students-list d-flex" style={{ marginTop: "10px" }}>
+        <div
+          className="students-list d-flex g-20"
+          style={{ marginTop: "10px", gap: "20px" }}>
           <div
             className="text-secondary-emphasis mb-3"
             style={{ fontWeight: "bolder", width: "200px" }}>
